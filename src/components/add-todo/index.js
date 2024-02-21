db12278c1aec86199d5013c51fda1f52d4613376
Princess Forson <pribeson121@gmail.com>
2024-02-21 05:42:41 -0800

    20/02/2024

--- src/components/add-todo/index.js
+++ src/components/add-todo/index.js
@@ -3,36 +3,38 @@ import { useLocalStorage } from "usehooks-ts";
 import { useState } from "react";
 
 function AddTodo() {
-    const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
-    // let todo;
-    const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
+  // let todo;
+  const [todo, setTodo] = useState("");
 
-    function collectInput(event) {
-        // todo = event.target.value;
-        setTodo(event.target.value);
-    }
+  function collectInput(event) {
+    // todo = event.target.value;
+    setTodo(event.target.value);
+  }
 
-    // const collectInput = (event) => 
-    //     setTodo(event.target.value);
-    // }
+  // const collectInput = (event) =>
+  //     setTodo(event.target.value);
+  // }
 
-    function saveTodo() {
-        // Add new todo to existing list of todos
-        // todos.push(todo);
-        // Set all todos in local storage
-        setTodos([...todos, todo]);
-    }
+  function saveTodo() {
+    // Add new todo to existing list of todos
+    // todos.push(todo);
+    // Set all todos in local storage
+    setTodos([...todos, todo]);
+  }
 
-
-    return (
-        <section className={styles.addTodo}>
-            <input
-                onChange={collectInput}
-                className={styles.addTodoInput}
-                placeholder="Start typing..." />
-            <button onClick={saveTodo}>Create</button>
-        </section>
-    );
+  return (
+    <section className={styles.addTodo}>
+      <input
+        onChange={collectInput}
+        className={styles.addTodoInput}
+        placeholder="Start typing..."
+      />
+      <button className="btn btn-danger" onClick={saveTodo}>
+        Create
+      </button>
+    </section>
+  );
 }
 
 export default AddTodo;
